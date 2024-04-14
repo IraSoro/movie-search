90ea7cb534685d42152782e3f2606576c72b473b
Irina Sorokina <irasoro402@yandex.ru>
2024-04-14 05:35:04 +0400

    Removed `Skeleton`

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -8,7 +8,6 @@ import {
   Box,
   Stack,
   Button,
-  Skeleton,
 } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import GradeIcon from "@mui/icons-material/Grade";
@@ -29,27 +28,13 @@ interface DetailsProps {
 const MovieDetails: React.FC<DetailsProps> = ({ movie }) => {
   return (
     <>
-      {movie.name ? (
-        <Typography
-          variant="h4"
-          component="h2"
-          sx={{ marginTop: "20px", marginBottom: "10px" }}
-        >
-          {movie.name || movie.alternativeName}
-        </Typography>
-      ) : (
-        <Stack
-          sx={{ marginTop: "20px", marginBottom: "10px" }}
-          spacing={2}
-        >
-          <Skeleton variant="rectangular"></Skeleton>
-          <Skeleton
-            variant="rectangular"
-            width="50%"
-          ></Skeleton>
-        </Stack>
-      )}
-
+      <Typography
+        variant="h4"
+        component="h2"
+        sx={{ marginTop: "20px", marginBottom: "10px" }}
+      >
+        {movie.name || movie.alternativeName}
+      </Typography>
       <Grid
         container
         spacing={2}
@@ -59,77 +44,53 @@ const MovieDetails: React.FC<DetailsProps> = ({ movie }) => {
           xs={12}
           md={6}
         >
-          {movie.poster.previewUrl ? (
-            <Card>
-              <Box
-                component="img"
-                sx={{
-                  height: "auto",
-                  maxWidth: "100%",
-                  display: "block",
-                  overflow: "hidden",
-                }}
-                src={movie.poster.previewUrl || "./assets/default.jpg"}
-                alt={`${movie.name} poster`}
-              />
-            </Card>
-          ) : (
-            <Skeleton
-              variant="rectangular"
-              width={350}
-              height={450}
+          <Card>
+            <Box
+              component="img"
+              sx={{
+                height: "auto",
+                maxWidth: "100%",
+                display: "block",
+                overflow: "hidden",
+              }}
+              src={movie.poster.previewUrl || "./assets/default.jpg"}
+              alt={`${movie.name} poster`}
             />
-          )}
+          </Card>
         </Grid>
         <Grid
           item
           xs={12}
           md={6}
         >
-          {movie.rating.kp ? (
-            <CardContent>
-              <Typography
-                variant="h6"
-                component="h3"
-                style={{ display: "flex", alignItems: "center" }}
-              >
-                <GradeIcon />
-                <span style={{ color: "#474d4e" }}>
-                  &nbsp;{movie.rating.kp}
-                </span>
-              </Typography>
-              <Typography
-                variant="h6"
-                component="h3"
-              >
-                Актеры:
-              </Typography>
-              <ActorsList />
-              {movie.type.includes("series") && (
-                <>
-                  <Typography
-                    variant="h6"
-                    component="h3"
-                  >
-                    Сезоны:
-                  </Typography>
-                  <SeasonsList />
-                </>
-              )}
-            </CardContent>
-          ) : (
-            <Stack spacing={2}>
-              <Skeleton
-                variant="rectangular"
-                width={100}
-              />
-              <Skeleton
-                variant="rectangular"
-                width={300}
-                height={200}
-              />
-            </Stack>
-          )}
+          <CardContent>
+            <Typography
+              variant="h6"
+              component="h3"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <GradeIcon />
+              <span style={{ color: "#474d4e" }}>&nbsp;{movie.rating.kp}</span>
+            </Typography>
+            <Typography
+              variant="h6"
+              component="h3"
+            >
+              Актеры:
+            </Typography>
+            <ActorsList />
+            {movie.type.includes("series") && (
+              <>
+                <Typography
+                  variant="h6"
+                  component="h3"
+                >
+                  Сезоны:
+                </Typography>
+                <SeasonsList />
+              </>
+            )}
+          </CardContent>
         </Grid>
       </Grid>
     </>
